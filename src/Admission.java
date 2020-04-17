96cb27f95e28d0dc884a2c01d12d064cb705ef7d
UsagiLiw <usagiliw@gmail.com>
2020-04-17 15:45:10 +0700

    Add Admission and component

---
+++ src/Admission.java
@@ -0,0 +1,78 @@
+import java.util.Date;
+
+public class Admission
+{
+    /**
+     * Date of admission
+     */
+    private Date admitDate;
+    /**
+     * List of symptoms in checkup
+     */
+    private SymptomList symptoms = new SymptomList();
+    /**
+     * List of treatment
+     */
+    private TreatmentList treatments = new TreatmentList();
+    /**
+     * List of lab test
+     */
+    private LabTestList labTests = new LabTestList();
+    /**
+     * List of medicine prescribe to this patient
+     */
+    private Prescriptions prescriptions = new Prescriptions();
+    /**
+     * Doctor's diagnosis of this patient checkup
+     */
+    private String diagnosis;
+    /**
+     * Name of assigned doctor
+     */
+    private String assignedDoctor;
+
+    /**
+     * Constructor
+     */
+    public Admission()
+    {
+    }
+
+
+
+
+    public Date getAdmitDate()
+    {
+        return admitDate;
+    }
+
+    public SymptomList getSymptoms()
+    {
+        return symptoms;
+    }
+
+    public TreatmentList getTreatments()
+    {
+        return treatments;
+    }
+
+    public LabTestList getLabTests()
+    {
+        return labTests;
+    }
+
+    public Prescriptions getPrescriptions()
+    {
+        return prescriptions;
+    }
+
+    public String getDiagnosis()
+    {
+        return diagnosis;
+    }
+
+    public String getAssignedDoctor()
+    {
+        return assignedDoctor;
+    }
+}
---
+++ src/LabTest.java
@@ -0,0 +1,21 @@
+public class LabTest
+{
+    private String labTestName;
+
+    private String result;
+
+    public LabTest()
+    {
+
+    }
+
+    public String getLabTestName()
+    {
+        return labTestName;
+    }
+
+    public String getResult()
+    {
+        return result;
+    }
+}
